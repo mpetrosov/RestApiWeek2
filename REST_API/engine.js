4d2c0c14c2451925819d92e3f991621136264a1a
Marat Petrosov <34557212+mpetrosov@users.noreply.github.com>
2018-01-25 23:47:06 +0100

    Update engine.js

--- REST_API/engine.js
+++ REST_API/engine.js
@@ -26,6 +26,7 @@ function generateApiListUrl(limit=1000, lastId=0) {
 
 // Make an async request to the API. Returns a new record.
 function apiCreate(fullname, comment) {
+    
     var url = generateApiCreateUrl();
     var request = new XMLHttpRequest();
     request.open('POST', url, false);  // `false` makes the request synchronous
@@ -47,6 +48,69 @@ function apiList(limit=1000, lastId=0) {
     }
 }
 
+
+// Create a new HTML child element - a single HTML representation of a single message
+function createChildElement(id, record) {
+    var elem = document.createElement('div');
+    elem.className = "message entry";
+    elem.id = "m" + id;
+    elem.innerHTML = "" + 
+    "<span class='name'> " + record.fullname + "</span>" +
+    "<p class='message'> " + record.comment + "</p>";
+    return elem;
+}
+
+
+// Save the address book record
+function saveAddressBookRecord(event) {
+    // The {event} we have here is the form "submit" event and by default it does a POST request
+    // to the same page, which results a page reload. I don't want that. What I want is on submit
+    // add the address book record to the local storage and add it to the bottom of the page.
+    event.preventDefault();
+
+    var addressBookRecordId = document.getElementById('record').value;
+
+    // Save into the database.
+    addressBookRecordId = apiCreate(
+        document.getElementById('fullname').value, 
+        document.getElementById('comment').value
+    );
+    // console.log(addressBookRecordId);
+    if (addressBookRecordId > lastId) {
+        lastId = addressBookRecordId;
+    }
+
+    var addressBookRecordData = {
+        fullname: document.getElementById('fullname').value,
+        comment: document.getElementById('comment').value
+    };
+
+    addressBookMessages[addressBookRecordId] = addressBookRecordData;
+
+    // Create new HTML element from data given
+    var child = createChildElement(addressBookRecordId, addressBookRecordData);
+
+    // Append the new element to the HTML DOM
+    messagesContainer.appendChild(child);
+
+    // Update the local storage
+    // apiCreate(
+    //     document.getElementById('fullname').value,
+    //     document.getElementById('comment').value
+    // )
+    // localStorage.setItem('address_book_messages', JSON.stringify(addressBookMessages));
+
+    // Reset form data
+    addressBookForm.reset();
+
+    document.getElementById('record').value = "";
+
+    // Scroll down to the last message
+    messagesContainer.scrollTop = messagesContainer.scrollHeight;
+}
+
+
+
 //make the field accessable only for numbers
 function isNumberKey(evt) {
    var charCode = (evt.which) ? evt.which : event.keyCode
@@ -56,8 +120,8 @@ function isNumberKey(evt) {
    return true;
 }
 
-// Start listening to DOM events when DOM content is laoded
-document.addEventListener('DOMContentLoaded', function() {
+// Main function to load the content
+function loadContent() {
 
     // On page load get all existing address book messages saved in the database.
     // If no messages or problem with loading the message, we return an empty object.
@@ -71,17 +135,6 @@ document.addEventListener('DOMContentLoaded', function() {
     // Just checking. If object is null or empty list, make an empty object of it.
     if (addressBookMessages == null || addressBookMessages == []) {
         addressBookMessages = {};
-    }
-    
-    // Create a new HTML child element - a single HTML representation of a single message
-    function createChildElement(id, record) {
-        var elem = document.createElement('div');
-        elem.className = "message entry";
-        elem.id = "m" + id;
-        elem.innerHTML = "" + 
-        "<span class='name'> " + record.fullname + "</span>" +
-        "<p class='message'> " + record.comment + "</p>";
-        return elem;
     }
 
     // This is the container which holds all the HTML messages
@@ -106,59 +159,18 @@ document.addEventListener('DOMContentLoaded', function() {
         messagesContainer.appendChild(child);
     }
 
+    // Scroll to the last message
+    messagesContainer.scrollTop = messagesContainer.scrollHeight;
+}
 
-    // Save the address book record
-    function saveAddressBookRecord(event) {
-        // The {event} we have here is the form "submit" event and by default it does a POST request
-        // to the same page, which results a page reload. I don't want that. What I want is on submit
-        // add the address book record to the local storage and add it to the bottom of the page.
-        event.preventDefault();
-
-        var addressBookRecordId = document.getElementById('record').value;
-
-        // Save into the database.
-        addressBookRecordId = apiCreate(
-            document.getElementById('fullname').value, 
-            document.getElementById('comment').value
-        );
-        // console.log(addressBookRecordId);
-        if (addressBookRecordId > lastId) {
-            lastId = addressBookRecordId;
-        }
-
-        var addressBookRecordData = {
-            fullname: document.getElementById('fullname').value,
-            comment: document.getElementById('comment').value
-        };
-
-        addressBookMessages[addressBookRecordId] = addressBookRecordData;
-
-        // Create new HTML element from data given
-        var child = createChildElement(addressBookRecordId, addressBookRecordData);
-
-        // Append the new element to the HTML DOM
-        messagesContainer.appendChild(child);
-
-        // Update the local storage
-        // apiCreate(
-        //     document.getElementById('fullname').value,
-        //     document.getElementById('comment').value
-        // )
-        // localStorage.setItem('address_book_messages', JSON.stringify(addressBookMessages));
-
-        // Reset form data
-        addressBookForm.reset();
-
-        document.getElementById('record').value = "";
-
-        // Scroll down
-        messagesContainer.scrollTop = messagesContainer.scrollHeight;
-    }
-
+// Start listening to DOM events when DOM content is laoded
+document.addEventListener('DOMContentLoaded', function() {
+    loadContent();
 
     document.getElementById('address-book-form').addEventListener('submit', saveAddressBookRecord);
     var allRemoveLinkElements = document.querySelectorAll('a.remove');
     
+    setInterval(loadContent, 2000);
 
 }, false);
 //}
